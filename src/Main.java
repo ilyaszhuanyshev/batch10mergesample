3dcf5464f1ea74272aae0008517785b5abdb3206
ilyas <zhuanyshev.ilyas@gmail.com>
2023-12-13 06:30:22 +0600

    MY SECOND COMMIT

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hi World");
+
+
+
+
+        int a_b = 22;
+
+        if(a_b==45){System.out.println("YES");}
+
     }
 }
